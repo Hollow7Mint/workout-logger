a54cc9ca2e5f97a5c31e3c11422fe1d7f04222ad
James Holloway <james.holloway94@gmail.com>
2026-04-28 11:29:27 +0000

    tag known_hosts sync checkpoint for key trail

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-04-28 10:19:32 UTC
+# Last sync: 2026-04-28 11:29:27 UTC
