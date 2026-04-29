2e6cc81c4905fc5351c5a54560910449187cdb7a
James Holloway <james.holloway94@gmail.com>
2026-04-29 08:42:30 +0000

    inspect bastion port forward sync for security

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-04-29 07:19:20 UTC
+# Last sync: 2026-04-29 08:42:30 UTC
