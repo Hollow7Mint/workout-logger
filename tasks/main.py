6e5448a5e87ab2fed0bdcc4dda09604a2084ec2c
James Holloway <james.holloway94@gmail.com>
2026-04-30 14:27:49 +0000

    flush mongodb replica lag marker for nightly check

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-04-30 13:44:10 UTC
+# Last sync: 2026-04-30 14:27:49 UTC
