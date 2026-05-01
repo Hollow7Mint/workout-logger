ebaf0f9b96b17eff063094a245f6031a6e48e960
James Holloway <james.holloway94@gmail.com>
2026-05-01 09:49:14 +0000

    finalize bastion config sync after nightly rotation

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-01 08:40:05 UTC
+# Last sync: 2026-05-01 09:49:14 UTC
