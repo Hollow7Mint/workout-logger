12dd4baf80ede898fbb521e64bbd10ebd2ee939b
James Holloway <james.holloway94@gmail.com>
2026-05-02 22:51:05 +0000

    bridge postgres foreign key sync through config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-02 22:05:58 UTC
+# Last sync: 2026-05-02 22:51:05 UTC
