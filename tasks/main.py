e357e0480e60ee8bda4d7034e68167cdc06fd09e
James Holloway <james.holloway94@gmail.com>
2026-05-03 00:50:36 +0000

    note postgres extension sync in config record

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-02 23:49:35 UTC
+# Last sync: 2026-05-03 00:50:36 UTC
