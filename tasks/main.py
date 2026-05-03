e7ef3244d930c992cdc1314995e8fa05b11dd511
James Holloway <james.holloway94@gmail.com>
2026-05-03 11:37:29 +0000

    adjust redis ttl sync stamp after cache flush

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-03 10:39:17 UTC
+# Last sync: 2026-05-03 11:37:29 UTC
