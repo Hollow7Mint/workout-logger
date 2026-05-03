4f13ac0d56fe57c930e677457164a6e16fbdf827
James Holloway <james.holloway94@gmail.com>
2026-05-03 09:23:05 +0000

    record kms key rotation time in config banner

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-03 08:33:01 UTC
+# Last sync: 2026-05-03 09:23:05 UTC
