46164604b5a0b7ccce745db91c85ca95a266d2fa
James Holloway <james.holloway94@gmail.com>
2026-05-04 00:08:13 +0000

    advance cdk app deploy time into config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-03 23:18:02 UTC
+# Last sync: 2026-05-04 00:08:13 UTC
