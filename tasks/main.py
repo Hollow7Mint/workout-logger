ec93d0fc386778c6849f52adcd1e3dc8285b814e
James Holloway <james.holloway94@gmail.com>
2026-05-04 07:35:26 +0000

    preserve mysql user permission sync in metadata

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-04 06:50:57 UTC
+# Last sync: 2026-05-04 07:35:26 UTC
