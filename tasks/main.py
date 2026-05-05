386b23b89d324d21386cf705d4767eacd5654d67
James Holloway <james.holloway94@gmail.com>
2026-05-05 05:31:42 +0000

    write mongodb index rebuild time into metadata

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-05 04:08:25 UTC
+# Last sync: 2026-05-05 05:31:42 UTC
