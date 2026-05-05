71a6b3bfdf9ca22c7d8302f780246cf5bda3a003
James Holloway <james.holloway94@gmail.com>
2026-05-05 11:49:04 +0000

    prime bastion connection sync marker after refresh

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-05 10:20:52 UTC
+# Last sync: 2026-05-05 11:49:04 UTC
