19e76a657d2956c1c1901465787c05c0a295926a
James Holloway <james.holloway94@gmail.com>
2026-05-07 16:01:22 +0000

    trace asg launch template time for fleet audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-07 14:57:44 UTC
+# Last sync: 2026-05-07 16:01:22 UTC
