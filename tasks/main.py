db4811180cf8f2ffd246d0bb61e52609dc6d1520
James Holloway <james.holloway94@gmail.com>
2026-05-09 18:09:46 +0000

    thread cloudtrail digest sync through config banner

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-09 17:24:29 UTC
+# Last sync: 2026-05-09 18:09:46 UTC
