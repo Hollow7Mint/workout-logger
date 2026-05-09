ec5173b67e144a99719a60becc8a46b1ddb45358
James Holloway <james.holloway94@gmail.com>
2026-05-09 05:35:39 +0000

    lift asg capacity sync time into deploy banner

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-09 04:31:54 UTC
+# Last sync: 2026-05-09 05:35:39 UTC
