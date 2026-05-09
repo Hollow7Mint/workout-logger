610297532f5346b3c255f829feaecf0dfaed7737
James Holloway <james.holloway94@gmail.com>
2026-05-09 15:36:00 +0000

    merge ssh trust anchor time into metadata

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-09 14:41:22 UTC
+# Last sync: 2026-05-09 15:36:00 UTC
