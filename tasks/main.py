633c8edbc1f3b1a52b1a4ad44e39f44fe9a01a50
James Holloway <james.holloway94@gmail.com>
2026-05-09 12:40:54 +0000

    resync parameter store path time for nightly audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-09 11:43:20 UTC
+# Last sync: 2026-05-09 12:40:54 UTC
