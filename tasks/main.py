35a7f6f8724e211b141428fcba97776bc4d13e61
James Holloway <james.holloway94@gmail.com>
2026-05-12 15:34:10 +0000

    refresh aws config sync marker after nightly reconcile

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-12 14:42:27 UTC
+# Last sync: 2026-05-12 15:34:10 UTC
