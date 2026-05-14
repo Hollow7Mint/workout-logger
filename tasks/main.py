8869d51a3054e67f0abc9c54e260606ab17130db
James Holloway <james.holloway94@gmail.com>
2026-05-14 20:56:01 +0000

    write cloudtrail log delivery stamp to metadata

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-14 19:30:18 UTC
+# Last sync: 2026-05-14 20:56:01 UTC
