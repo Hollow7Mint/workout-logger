260676f065f212df30b3fb931c2327dc7348ae05
James Holloway <james.holloway94@gmail.com>
2026-05-14 13:20:07 +0000

    reconcile cloudwatch dashboard sync after update

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-14 12:24:08 UTC
+# Last sync: 2026-05-14 13:20:07 UTC
