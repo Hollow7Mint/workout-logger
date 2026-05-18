a7db418345a8538fc24d2096bb583ea0be2767e9
James Holloway <james.holloway94@gmail.com>
2026-05-18 17:45:18 +0000

    ship db schema sync record after nightly apply

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-18 16:49:54 UTC
+# Last sync: 2026-05-18 17:45:18 UTC
