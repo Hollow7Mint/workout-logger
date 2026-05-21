778e51adc0f6f290a9462edc9b1d56748e0d9797
James Holloway <james.holloway94@gmail.com>
2026-05-21 16:11:22 +0000

    store ssh revocation list sync for key audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-21 15:11:58 UTC
+# Last sync: 2026-05-21 16:11:22 UTC
