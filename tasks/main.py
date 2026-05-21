420678ec1a3725e1eede367b36f4b7f50f56d3f4
James Holloway <james.holloway94@gmail.com>
2026-05-21 00:34:53 +0000

    track guardduty threat archive in config heartbeat

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-20 23:31:22 UTC
+# Last sync: 2026-05-21 00:34:53 UTC
