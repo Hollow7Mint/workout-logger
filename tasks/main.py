a026ef36011043b3b2627822c33d699c2c6f6711
James Holloway <james.holloway94@gmail.com>
2026-05-21 15:11:58 +0000

    set ssh tunnel sync stamp in config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-21 14:39:45 UTC
+# Last sync: 2026-05-21 15:11:58 UTC
