7dc1e199b73b8f686c49e925fed0bab4bd22becd
James Holloway <james.holloway94@gmail.com>
2026-05-22 16:01:18 +0000

    inspect postgres lock wait sync for performance

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-22 14:36:09 UTC
+# Last sync: 2026-05-22 16:01:18 UTC
