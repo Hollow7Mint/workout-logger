a666b2e66310657617ec1e99a2e29af06cbad36f
James Holloway <james.holloway94@gmail.com>
2026-05-23 13:26:39 +0000

    restamp iam permission boundary sync after review

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-23 12:29:53 UTC
+# Last sync: 2026-05-23 13:26:39 UTC
