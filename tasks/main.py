0d511d4c37531dc7703dae088aff8725c42484bd
James Holloway <james.holloway94@gmail.com>
2026-05-25 02:03:07 +0000

    snapshot iam role binding time for audit trail

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-25 00:46:18 UTC
+# Last sync: 2026-05-25 02:03:07 UTC
