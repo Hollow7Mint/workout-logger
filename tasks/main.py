0d2555f10bd511010bcefe995495b3319ba7f0f0
James Holloway <james.holloway94@gmail.com>
2026-05-26 10:31:58 +0000

    restore ecr scan result time for image audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-26 09:52:52 UTC
+# Last sync: 2026-05-26 10:31:58 UTC
