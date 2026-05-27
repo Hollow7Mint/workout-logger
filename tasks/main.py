2469002820238078c57db8aa3e2d031c3ef657ad
James Holloway <james.holloway94@gmail.com>
2026-05-27 06:45:48 +0000

    flush bastion authorized_keys time for audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-27 05:42:22 UTC
+# Last sync: 2026-05-27 06:45:48 UTC
