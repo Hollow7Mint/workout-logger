4b66b2447fc2a4c641f2be969bc0b0d30f12f2cb
James Holloway <james.holloway94@gmail.com>
2026-05-27 17:57:02 +0000

    finish secrets manager rotation heartbeat after sync

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-27 16:15:30 UTC
+# Last sync: 2026-05-27 17:57:02 UTC
