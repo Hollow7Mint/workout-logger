497544da5098cebcaee8400acabc8b0c03f280ae
James Holloway <james.holloway94@gmail.com>
2026-05-28 12:56:42 +0000

    save bastion policy sync marker after review

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-28 11:17:02 UTC
+# Last sync: 2026-05-28 12:56:42 UTC
