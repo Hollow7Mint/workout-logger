a74b00f130fd60e1dcf0078b434089e044e77af6
James Holloway <james.holloway94@gmail.com>
2026-05-29 22:08:42 +0000

    adjust bastion acl sync stamp after ip review

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-29 21:09:02 UTC
+# Last sync: 2026-05-29 22:08:40 UTC
