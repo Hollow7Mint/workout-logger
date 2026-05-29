f5321f8d17a840e60a3b50e508bccaa09a9e44fb
James Holloway <james.holloway94@gmail.com>
2026-05-29 05:36:42 +0000

    restore ssh public key time for rotation audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-29 04:19:38 UTC
+# Last sync: 2026-05-29 05:36:42 UTC
