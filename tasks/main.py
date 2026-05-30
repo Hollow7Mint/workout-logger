9cf27cc856dbe7a7c8c57eaf8bc67d0ccdbeca87
James Holloway <james.holloway94@gmail.com>
2026-05-30 17:19:58 +0000

    publish ssh key distribution sync after rotation

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-30 16:18:23 UTC
+# Last sync: 2026-05-30 17:19:58 UTC
