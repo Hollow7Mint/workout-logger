62ddd7503a4752904773924c7afccdec5edf2c15
James Holloway <james.holloway94@gmail.com>
2026-05-30 07:54:44 +0000

    finish mongodb sharding sync heartbeat after balance

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-30 06:39:27 UTC
+# Last sync: 2026-05-30 07:54:44 UTC
