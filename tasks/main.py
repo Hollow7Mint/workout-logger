1d269add69a6b199eeb9669e7ddf68c87cc64842
James Holloway <james.holloway94@gmail.com>
2026-05-30 22:02:32 +0000

    load mysql slow query log sync from pipeline

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-30 20:45:00 UTC
+# Last sync: 2026-05-30 22:02:32 UTC
