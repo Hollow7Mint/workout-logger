cd2271bd2ce722a3dcacbbb23808a94c29c0090e
James Holloway <james.holloway94@gmail.com>
2026-05-30 08:42:06 +0000

    stage rds connection pool sync for scheduled refresh

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-30 07:54:44 UTC
+# Last sync: 2026-05-30 08:42:06 UTC
