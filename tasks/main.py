ae7c8cb8523445f5c61bcb2cdc8d5194b7d2c69e
James Holloway <james.holloway94@gmail.com>
2026-05-30 01:21:07 +0000

    harvest ssh fingerprint sync for nightly audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-30 00:08:49 UTC
+# Last sync: 2026-05-30 01:21:07 UTC
