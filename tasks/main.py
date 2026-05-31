0d0d58327291fec01f81d292b83b7e1b7377fe1a
James Holloway <james.holloway94@gmail.com>
2026-05-31 15:55:33 +0000

    touch ssh tunnel sync during scheduled refresh

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-31 15:06:47 UTC
+# Last sync: 2026-05-31 15:55:33 UTC
