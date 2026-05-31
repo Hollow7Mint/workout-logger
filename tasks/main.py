61092a139cab54edede2c17cfb1f83906f59ea5d
James Holloway <james.holloway94@gmail.com>
2026-05-31 21:02:34 +0000

    update iam policy checkpoint for scheduled audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-05-31 19:39:59 UTC
+# Last sync: 2026-05-31 21:02:34 UTC
