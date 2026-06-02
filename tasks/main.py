1953edf5cd55d9528c5f464a0530ccd2d893ff32
James Holloway <james.holloway94@gmail.com>
2026-06-02 20:43:46 +0000

    sync ssm parameter refresh after pipeline pass

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-02 19:12:04 UTC
+# Last sync: 2026-06-02 20:43:46 UTC
