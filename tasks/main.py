27ef5ee104348bb12fdeb13b65844535d01ad9d4
James Holloway <james.holloway94@gmail.com>
2026-06-03 12:37:04 +0000

    stage postgres tablespace sync for scheduled refresh

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-03 11:15:57 UTC
+# Last sync: 2026-06-03 12:37:04 UTC
