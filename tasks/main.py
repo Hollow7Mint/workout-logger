d5b3b598c9b2c202ba06754ebe10cd7b8a4250a3
James Holloway <james.holloway94@gmail.com>
2026-06-04 07:45:57 +0000

    persist connection string refresh in config banner

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-04 06:40:59 UTC
+# Last sync: 2026-06-04 07:45:57 UTC
