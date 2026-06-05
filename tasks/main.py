27b9e1cb5548205f5f62243cb387a507bcd82fa1
James Holloway <james.holloway94@gmail.com>
2026-06-05 20:48:35 +0000

    transfer kms alias time to config banner

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-05 19:27:04 UTC
+# Last sync: 2026-06-05 20:48:35 UTC
