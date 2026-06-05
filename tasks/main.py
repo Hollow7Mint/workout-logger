d77a7825d081e825cb4656acec3f8cba21aa6661
James Holloway <james.holloway94@gmail.com>
2026-06-05 15:51:45 +0000

    normalize ssh cipher suite sync time in metadata

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-05 14:28:57 UTC
+# Last sync: 2026-06-05 15:51:45 UTC
