e5a66deeb5d48edce5d25c718d4b52ab4f063b8f
James Holloway <james.holloway94@gmail.com>
2026-06-05 07:11:11 +0000

    finalize vpc security group sync after rule push

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-05 06:53:15 UTC
+# Last sync: 2026-06-05 07:11:11 UTC
