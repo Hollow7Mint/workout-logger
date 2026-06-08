027623d1a905df756ed86433d580624430691afe
James Holloway <james.holloway94@gmail.com>
2026-06-08 15:34:33 +0000

    retain redis aof sync stamp for persistence audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-08 15:03:20 UTC
+# Last sync: 2026-06-08 15:34:33 UTC
