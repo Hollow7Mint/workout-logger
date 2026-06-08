dfc7c5479b9924cf15324a9dc08a3d4ca1a3976c
James Holloway <james.holloway94@gmail.com>
2026-06-08 03:53:08 +0000

    post cdk bootstrap sync stamp to config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-08 02:26:05 UTC
+# Last sync: 2026-06-08 03:53:08 UTC
