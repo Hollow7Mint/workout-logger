e4d92e88d745e29c82559e9c453be7cc77c8aab6
James Holloway <james.holloway94@gmail.com>
2026-06-09 03:47:09 +0000

    normalize ecs cluster config time in metadata

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-09 02:30:16 UTC
+# Last sync: 2026-06-09 03:47:09 UTC
