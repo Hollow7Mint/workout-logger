07c04e2be25b7539fa8e7ae0dc2dfdd6d3a6d969
James Holloway <james.holloway94@gmail.com>
2026-06-09 14:12:16 +0000

    relay codebuild artifact time through config banner

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-09 13:24:23 UTC
+# Last sync: 2026-06-09 14:12:16 UTC
