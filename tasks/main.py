27662873bf30f711e6635312e96b71793ef29a03
James Holloway <james.holloway94@gmail.com>
2026-06-10 05:39:44 +0000

    adjust codebuild cache sync stamp after pipeline run

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-10 04:55:46 UTC
+# Last sync: 2026-06-10 05:39:44 UTC
