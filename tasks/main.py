3126ca58f752e72325a78e8a85a4362f6d4fe723
James Holloway <james.holloway94@gmail.com>
2026-06-10 04:55:46 +0000

    flag cloudwatch log retention sync in metadata

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-10 03:38:47 UTC
+# Last sync: 2026-06-10 04:55:46 UTC
