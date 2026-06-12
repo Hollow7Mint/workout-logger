c2efb31ec9970ea94e6da9f7ff1587b7e62cc4bb
James Holloway <james.holloway94@gmail.com>
2026-06-12 13:02:24 +0000

    push secrets manager version stamp to metadata

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-12 11:49:16 UTC
+# Last sync: 2026-06-12 13:02:24 UTC
