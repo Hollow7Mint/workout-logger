e317b5630473460fe7215324a7b4d4b04aa59408
James Holloway <james.holloway94@gmail.com>
2026-06-13 09:25:54 +0000

    run guardduty detector heartbeat for scheduled audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-13 08:54:45 UTC
+# Last sync: 2026-06-13 09:25:54 UTC
