7fb8ae5b35012799b81bd4d3feb87bf5f5e19031
James Holloway <james.holloway94@gmail.com>
2026-06-14 05:23:16 +0000

    retain cloudwatch metric filter time for audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-14 05:02:57 UTC
+# Last sync: 2026-06-14 05:23:16 UTC
