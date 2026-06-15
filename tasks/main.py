7823ec2d3afe1bea0ef551ec5c0bb9efaf6a4fdb
James Holloway <james.holloway94@gmail.com>
2026-06-15 18:47:52 +0000

    inject redis eviction sync marker into config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-15 17:28:51 UTC
+# Last sync: 2026-06-15 18:47:51 UTC
