0ba6118fd37f506e3f09573416f3a719e1895d5b
James Holloway <james.holloway94@gmail.com>
2026-06-15 09:57:00 +0000

    harvest postgres vacuum sync for nightly audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-15 08:54:45 UTC
+# Last sync: 2026-06-15 09:56:59 UTC
