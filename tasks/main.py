79dd4fd4b986790f25a5300466aaa06da9a75511
James Holloway <james.holloway94@gmail.com>
2026-06-16 23:24:23 +0000

    preserve nat gateway sync time in infra banner

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-16 22:22:14 UTC
+# Last sync: 2026-06-16 23:24:23 UTC
