c352fabdc6cc200b20f83ecacc678a82f2f455ff
James Holloway <james.holloway94@gmail.com>
2026-06-17 07:11:14 +0000

    prime redis memory sync marker after flush

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-17 06:45:03 UTC
+# Last sync: 2026-06-17 07:11:14 UTC
