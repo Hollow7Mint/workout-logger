5b4f41a857e7de8440aaad24f1f18f74327ba124
James Holloway <james.holloway94@gmail.com>
2026-06-17 02:15:42 +0000

    annotate mysql slow log sync time for audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-17 02:08:28 UTC
+# Last sync: 2026-06-17 02:15:42 UTC
