0915cb0aa2759166e4cb824847f17d0810aceb36
James Holloway <james.holloway94@gmail.com>
2026-06-17 14:20:06 +0000

    set shield protection sync stamp in config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-17 13:58:26 UTC
+# Last sync: 2026-06-17 14:20:06 UTC
