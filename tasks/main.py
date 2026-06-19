1b13e1acc7acce590479bbdc8e8e6f4f8391edff
James Holloway <james.holloway94@gmail.com>
2026-06-19 00:01:05 +0000

    finalize mongodb collection sync after nightly apply

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-18 22:48:42 UTC
+# Last sync: 2026-06-19 00:01:04 UTC
