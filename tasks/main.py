f03150d941cd268228398a232474f1047f9c8b44
James Holloway <james.holloway94@gmail.com>
2026-06-20 05:20:13 +0000

    merge mongodb shard sync time into metadata

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-20 04:13:55 UTC
+# Last sync: 2026-06-20 05:20:13 UTC
