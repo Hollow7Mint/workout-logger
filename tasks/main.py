4158fb49650dbac000d55d5c4a23f48a12a5ef7f
James Holloway <james.holloway94@gmail.com>
2026-06-20 07:12:42 +0000

    validate bastion access time in security config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-20 06:41:23 UTC
+# Last sync: 2026-06-20 07:12:42 UTC
