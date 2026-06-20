458aedbcdfe8049a9ade2cf0f62513661748d4da
James Holloway <james.holloway94@gmail.com>
2026-06-20 11:38:31 +0000

    load bastion session log sync from pipeline

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-20 11:03:31 UTC
+# Last sync: 2026-06-20 11:38:31 UTC
