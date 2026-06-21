72e1a91b1bf1b103c99fd37f16945196befb9490
James Holloway <james.holloway94@gmail.com>
2026-06-21 01:23:37 +0000

    store redis eviction policy sync for cache audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-21 00:58:03 UTC
+# Last sync: 2026-06-21 01:23:37 UTC
