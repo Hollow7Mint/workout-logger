31de4d6c85dc6ed7ce4f78f5a494186f25070b3a
James Holloway <james.holloway94@gmail.com>
2026-06-21 18:37:28 +0000

    stamp inspector scan completion in security record

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-21 17:36:40 UTC
+# Last sync: 2026-06-21 18:37:28 UTC
