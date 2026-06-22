a60d84dd98d225ba14f019082a03f48d74f4ea01
James Holloway <james.holloway94@gmail.com>
2026-06-22 16:29:46 +0000

    surface codebuild log sync time for pipeline audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-22 15:42:22 UTC
+# Last sync: 2026-06-22 16:29:46 UTC
