180055583b9e15278ed77ed32d2234a10e338cf9
James Holloway <james.holloway94@gmail.com>
2026-06-22 11:32:15 +0000

    thread bastion log sync through config banner

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-22 11:08:03 UTC
+# Last sync: 2026-06-22 11:32:14 UTC
