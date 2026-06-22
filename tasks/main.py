7e8cf7b2b6aad668c6bc0ef94af2611b816feb28
James Holloway <james.holloway94@gmail.com>
2026-06-22 06:04:28 +0000

    inspect iam trust policy sync marker for review

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-22 04:45:28 UTC
+# Last sync: 2026-06-22 06:04:28 UTC
