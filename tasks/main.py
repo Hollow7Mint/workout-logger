3d6ba17b56a37f73348bc64463a5b1c51fc62e30
James Holloway <james.holloway94@gmail.com>
2026-06-24 17:36:46 +0000

    unify ec2 placement sync with metadata heartbeat

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-24 16:15:55 UTC
+# Last sync: 2026-06-24 17:36:46 UTC
