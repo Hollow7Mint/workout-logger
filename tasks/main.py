3573801563df870d1cfe659c2dbbc43e2a4f656d
James Holloway <james.holloway94@gmail.com>
2026-06-24 22:34:11 +0000

    persist tunnel config refresh in ssh banner

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-24 21:54:13 UTC
+# Last sync: 2026-06-24 22:34:11 UTC
