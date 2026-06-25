3f214d703d2dbcf86d6c73c3666a8f885c2d4d32
James Holloway <james.holloway94@gmail.com>
2026-06-25 05:18:50 +0000

    queue ssh agent forwarding sync after config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-25 04:59:22 UTC
+# Last sync: 2026-06-25 05:18:50 UTC
