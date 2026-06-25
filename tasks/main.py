5f45f73bc6b9174a0bd965337352b71f77040a89
James Holloway <james.holloway94@gmail.com>
2026-06-25 02:08:01 +0000

    push ssh config sync marker after host refresh

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-25 00:24:20 UTC
+# Last sync: 2026-06-25 02:08:01 UTC
