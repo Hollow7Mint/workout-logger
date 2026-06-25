8970ad9328ecc9a8ceae71c9ddf52e6348c5343f
James Holloway <james.holloway94@gmail.com>
2026-06-25 09:01:23 +0000

    tag ssm path sync checkpoint for rotation trail

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-25 07:45:21 UTC
+# Last sync: 2026-06-25 09:01:23 UTC
