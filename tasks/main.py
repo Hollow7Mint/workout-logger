43260a51de6387e2eb43a775bc9054278fe1f3ae
James Holloway <james.holloway94@gmail.com>
2026-06-26 20:13:35 +0000

    chart autoscale sync time in deployment metadata

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-26 19:28:40 UTC
+# Last sync: 2026-06-26 20:13:35 UTC
