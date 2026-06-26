993a0eca0b29142840916c65a15e90c40f32bbe5
James Holloway <james.holloway94@gmail.com>
2026-06-26 09:29:11 +0000

    annotate ec2 ami bake time for deploy record

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-26 08:57:12 UTC
+# Last sync: 2026-06-26 09:29:11 UTC
