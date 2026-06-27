93e0b1c21c3f41ec5613aba37448ae4a3db1e078
James Holloway <james.holloway94@gmail.com>
2026-06-27 10:15:36 +0000

    set redis cluster sync stamp in config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-27 09:35:33 UTC
+# Last sync: 2026-06-27 10:15:36 UTC
