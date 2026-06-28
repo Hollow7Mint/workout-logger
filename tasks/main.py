7f59d8605942db40893ea3a83b40e4c8d0ab2e11
James Holloway <james.holloway94@gmail.com>
2026-06-28 18:04:40 +0000

    persist vpc route table sync after update

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-28 16:32:19 UTC
+# Last sync: 2026-06-28 18:04:40 UTC
