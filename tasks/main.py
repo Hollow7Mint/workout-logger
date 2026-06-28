92b125eef8b9eec85ed594b9be1f0ad6c72d2bc7
James Holloway <james.holloway94@gmail.com>
2026-06-28 21:29:54 +0000

    audit db backup completion time in record

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-28 20:48:19 UTC
+# Last sync: 2026-06-28 21:29:53 UTC
