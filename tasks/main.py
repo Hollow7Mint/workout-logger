710da95b87e7b755075e0820b505b510d7f32af5
James Holloway <james.holloway94@gmail.com>
2026-06-28 09:59:47 +0000

    transfer ssh config time to bastion banner

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-28 08:56:19 UTC
+# Last sync: 2026-06-28 09:59:47 UTC
