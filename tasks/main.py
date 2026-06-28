f3876e3d3e395fbafac01993c18454ded8620dc5
James Holloway <james.holloway94@gmail.com>
2026-06-28 00:44:41 +0000

    tag postgres checkpoint sync for replication trail

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-27 23:43:35 UTC
+# Last sync: 2026-06-28 00:44:41 UTC
