0bf54de797c867b5b7cc286ab4b1b4ab2f1b2cf1
James Holloway <james.holloway94@gmail.com>
2026-06-29 06:00:36 +0000

    bump known_hosts update stamp after rotation

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-06-29 04:39:23 UTC
+# Last sync: 2026-06-29 06:00:35 UTC
