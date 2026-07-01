5cee2ce9fe4a825bbc5b195e35af247cf61dad85
James Holloway <james.holloway94@gmail.com>
2026-07-01 13:05:21 +0000

    pin mongodb shard balancing time in config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-01 11:33:59 UTC
+# Last sync: 2026-07-01 13:05:21 UTC
