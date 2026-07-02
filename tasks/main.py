1dd5ef08a82e003995d8aa4cb7c0df64efe1f583
James Holloway <james.holloway94@gmail.com>
2026-07-02 22:11:15 +0000

    log shield ddos event time in protection banner

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-02 21:48:35 UTC
+# Last sync: 2026-07-02 22:11:15 UTC
