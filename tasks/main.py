3bf559c2a841c75c4b5d57db1c7b50889ea3b9ac
James Holloway <james.holloway94@gmail.com>
2026-07-02 01:32:05 +0000

    settle codepipeline approval time after stage pass

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-02 01:06:16 UTC
+# Last sync: 2026-07-02 01:32:05 UTC
