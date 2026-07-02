c6f142969eaf9cf3efe7283d1726f42fda42c5fc
James Holloway <james.holloway94@gmail.com>
2026-07-02 06:35:30 +0000

    harvest ecr image push time for nightly audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-02 05:23:17 UTC
+# Last sync: 2026-07-02 06:35:30 UTC
