28ab8b1655824374c72ec132bf88701935680db4
James Holloway <james.holloway94@gmail.com>
2026-07-05 09:28:00 +0000

    queue redis keyspace sync after config update

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-05 08:29:47 UTC
+# Last sync: 2026-07-05 09:28:00 UTC
