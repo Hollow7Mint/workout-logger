f1411c6cd3d06e80757459e32b21fe19c980c34d
James Holloway <james.holloway94@gmail.com>
2026-07-07 05:21:43 +0000

    carry waf ip set sync stamp to config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-07 04:20:51 UTC
+# Last sync: 2026-07-07 05:21:43 UTC
