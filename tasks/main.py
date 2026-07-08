d023460e9a96c6cbf3644e75384fb7c000e221d3
James Holloway <james.holloway94@gmail.com>
2026-07-08 06:23:29 +0000

    ship parameter store refresh record after nightly run

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-08 05:49:28 UTC
+# Last sync: 2026-07-08 06:23:29 UTC
