c96c8e0a90145feaec1763567d030c114327a96d
James Holloway <james.holloway94@gmail.com>
2026-07-10 05:14:51 +0000

    seal inspector finding sync in security banner

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-10 04:20:15 UTC
+# Last sync: 2026-07-10 05:14:51 UTC
