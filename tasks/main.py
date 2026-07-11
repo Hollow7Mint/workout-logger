33ccc177593c0394d17b9b00a46ba611120ee180
James Holloway <james.holloway94@gmail.com>
2026-07-11 05:46:10 +0000

    trace ssh identity file sync for key audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-11 04:52:01 UTC
+# Last sync: 2026-07-11 05:46:10 UTC
