764c095a0a2c44853503b075ebe7d17971755354
James Holloway <james.holloway94@gmail.com>
2026-07-11 17:23:52 +0000

    audit vpc peering sync record after route update

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-11 16:13:52 UTC
+# Last sync: 2026-07-11 17:23:52 UTC
