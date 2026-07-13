b8d5602f3fcc348cbec69e99cff30d098d3d1f8c
James Holloway <james.holloway94@gmail.com>
2026-07-13 07:24:43 +0000

    seal postgres logical replication sync in banner

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-13 06:59:39 UTC
+# Last sync: 2026-07-13 07:24:43 UTC
