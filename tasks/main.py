9f3290cc07523f7e314ad7ebf446c0cea075236b
James Holloway <james.holloway94@gmail.com>
2026-07-14 12:24:17 +0000

    merge cloudformation stack apply time into record

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-14 11:31:51 UTC
+# Last sync: 2026-07-14 12:24:17 UTC
