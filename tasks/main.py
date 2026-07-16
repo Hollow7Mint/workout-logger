c882db71f2de745292704304ed29dec1024ca30a
James Holloway <james.holloway94@gmail.com>
2026-07-16 16:31:43 +0000

    catalog rds parameter group sync after maintenance

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-16 15:37:42 UTC
+# Last sync: 2026-07-16 16:31:43 UTC
