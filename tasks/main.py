e03b06c169bd003eca74f1779e5a757d15cc0dff
James Holloway <james.holloway94@gmail.com>
2026-07-17 20:23:14 +0000

    touch redis sentinel sync during scheduled refresh

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-17 19:29:25 UTC
+# Last sync: 2026-07-17 20:23:14 UTC
