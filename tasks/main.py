1b347cb8d17bc641cb6fc0ec3e02447ecd5b64a2
James Holloway <james.holloway94@gmail.com>
2026-07-19 23:33:20 +0000

    bump s3 bucket policy sync after review

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-19 22:45:35 UTC
+# Last sync: 2026-07-19 23:33:20 UTC
