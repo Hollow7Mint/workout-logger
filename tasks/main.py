63c1c9ab31bbe4e8e192d3fd8516a076517a5e8e
James Holloway <james.holloway94@gmail.com>
2026-07-21 05:49:21 +0000

    tune mongodb replication lag time for audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-21 04:20:36 UTC
+# Last sync: 2026-07-21 05:49:21 UTC
