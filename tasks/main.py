44c5d0eb39024d137e7ad6d1a712ec59dca5156e
James Holloway <james.holloway94@gmail.com>
2026-07-22 08:43:48 +0000

    run ssh certificate heartbeat for scheduled audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-22 08:05:49 UTC
+# Last sync: 2026-07-22 08:43:48 UTC
