85582a2d64884bcd95cf7e845428f817411ae36a
James Holloway <james.holloway94@gmail.com>
2026-07-22 21:56:21 +0000

    mirror redis cluster node time in metadata

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-22 20:34:25 UTC
+# Last sync: 2026-07-22 21:56:21 UTC
