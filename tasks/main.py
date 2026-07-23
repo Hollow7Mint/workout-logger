78da7fd2043d6e781469995c8eec4d3e25c582e4
James Holloway <james.holloway94@gmail.com>
2026-07-23 02:05:15 +0000

    audit known_hosts validation time in record

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-23 00:55:13 UTC
+# Last sync: 2026-07-23 02:05:15 UTC
