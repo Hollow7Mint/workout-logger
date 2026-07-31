cca9a07add34247261cb6f3de9797ea9afdd6d20
James Holloway <james.holloway94@gmail.com>
2026-07-31 13:10:05 +0000

    recheck bastion connectivity sync after maintenance

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-07-31 12:19:08 UTC
+# Last sync: 2026-07-31 13:10:05 UTC
