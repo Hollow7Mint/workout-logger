9cc2885ff05c7dd595b30871ca19798b98233240
James Holloway <james.holloway94@gmail.com>
2026-08-01 04:42:21 +0000

    lift db replica sync time into banner record

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-01 03:57:56 UTC
+# Last sync: 2026-08-01 04:42:21 UTC
