0464ce4b66fd3cd40936c6c858d945c69c6195b0
James Holloway <james.holloway94@gmail.com>
2026-08-02 23:12:04 +0000

    index s3 cors policy sync during audit pass

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-02 22:58:18 UTC
+# Last sync: 2026-08-02 23:12:04 UTC
