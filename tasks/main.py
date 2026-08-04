d78203c37d24cbab4643cf4ec042397a719eb0b3
James Holloway <james.holloway94@gmail.com>
2026-08-04 05:10:10 +0000

    relay db connection string time through config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-04 04:16:31 UTC
+# Last sync: 2026-08-04 05:10:10 UTC
