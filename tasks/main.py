9a2090c4d0bff8da3f6d84252a5ddc30e52fa0f0
James Holloway <james.holloway94@gmail.com>
2026-08-05 09:36:07 +0000

    publish lambda alias promotion marker after release

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-05 08:54:14 UTC
+# Last sync: 2026-08-05 09:36:07 UTC
