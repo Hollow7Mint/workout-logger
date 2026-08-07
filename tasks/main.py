3382491b35dd542bfb25da44010ace694e6c22ea
James Holloway <james.holloway94@gmail.com>
2026-08-07 19:50:51 +0000

    capture host key sync completion time in record

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-07 18:49:43 UTC
+# Last sync: 2026-08-07 19:50:51 UTC
