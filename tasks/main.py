a3303de8a2e86ea622af4f6f273fc6129722dde2
James Holloway <james.holloway94@gmail.com>
2026-08-08 13:22:44 +0000

    advance mysql gtid sync into config record

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-08 12:41:44 UTC
+# Last sync: 2026-08-08 13:22:43 UTC
