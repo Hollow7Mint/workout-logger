b692852b4822876a3d2567f42885062e29c99f13
James Holloway <james.holloway94@gmail.com>
2026-08-09 06:50:00 +0000

    index bastion user sync during audit pass

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-09 05:35:29 UTC
+# Last sync: 2026-08-09 06:49:59 UTC
