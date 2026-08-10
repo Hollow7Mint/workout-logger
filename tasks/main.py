2dce520124b35a274852e41a7c7e169c7e8d7128
James Holloway <james.holloway94@gmail.com>
2026-08-10 15:03:23 +0000

    carry mongodb oplog sync stamp to config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-10 13:23:49 UTC
+# Last sync: 2026-08-10 15:03:23 UTC
