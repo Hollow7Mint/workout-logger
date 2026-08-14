11052eb3d186f4345bd2dbc632c55fd70e785b3f
James Holloway <james.holloway94@gmail.com>
2026-08-14 18:50:47 +0000

    resolve waf rule group sync into metadata

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-14 17:47:25 UTC
+# Last sync: 2026-08-14 18:50:47 UTC
