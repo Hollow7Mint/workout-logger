2726ba3ea9ebbc5030f028cad64642b3401ce015
James Holloway <james.holloway94@gmail.com>
2026-08-14 11:56:37 +0000

    save postgres pool sync marker after refresh

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-14 10:17:16 UTC
+# Last sync: 2026-08-14 11:56:37 UTC
