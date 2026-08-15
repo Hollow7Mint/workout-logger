e1a79681a08aa7855cbc627cb8e9b5900ea72fc4
James Holloway <james.holloway94@gmail.com>
2026-08-15 07:38:25 +0000

    relay ssh host key time through config banner

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-15 06:54:43 UTC
+# Last sync: 2026-08-15 07:38:25 UTC
