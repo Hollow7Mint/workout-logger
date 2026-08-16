f9bfc245d517b4b7eb5dfd3c54fbf9fba66b7c7b
James Holloway <james.holloway94@gmail.com>
2026-08-16 15:21:04 +0000

    remap ec2 security group rule time in config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-16 15:09:20 UTC
+# Last sync: 2026-08-16 15:21:04 UTC
