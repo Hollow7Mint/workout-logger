6a0bd1ad010d45359cc27d58a7670788dd9ec018
James Holloway <james.holloway94@gmail.com>
2026-08-17 02:00:27 +0000

    capture rds snapshot completion time in record

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-17 00:41:29 UTC
+# Last sync: 2026-08-17 02:00:27 UTC
