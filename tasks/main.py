d50f651e6aff35d26d7da5597dd0da3ad59b953c
James Holloway <james.holloway94@gmail.com>
2026-08-17 16:19:31 +0000

    frame ssm document sync time in config record

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-17 15:32:51 UTC
+# Last sync: 2026-08-17 16:19:31 UTC
