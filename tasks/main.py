9f633e2777db8b971e114cb87acc2b48990fdafb
James Holloway <james.holloway94@gmail.com>
2026-08-20 18:11:32 +0000

    pin bastion ip allowlist time in config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-20 17:20:23 UTC
+# Last sync: 2026-08-20 18:11:32 UTC
