1c9400f205f759297c0ddfbdba966b1e5c7d72f0
James Holloway <james.holloway94@gmail.com>
2026-08-20 22:23:18 +0000

    post ssh key rotation heartbeat to config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-20 21:26:03 UTC
+# Last sync: 2026-08-20 22:23:18 UTC
