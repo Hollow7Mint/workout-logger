687d927b61ccdfc6d05919d6c8ad40536edea3ee
James Holloway <james.holloway94@gmail.com>
2026-08-22 22:01:47 +0000

    resolve mysql backup sync into metadata file

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-22 20:25:08 UTC
+# Last sync: 2026-08-22 22:01:46 UTC
