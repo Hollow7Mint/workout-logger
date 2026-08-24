a8be57abd75b6ae296f5a9ff72d18b61910fb371
James Holloway <james.holloway94@gmail.com>
2026-08-24 17:13:27 +0000

    rotate kms key metadata sync after policy apply

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-24 16:14:37 UTC
+# Last sync: 2026-08-24 17:13:27 UTC
