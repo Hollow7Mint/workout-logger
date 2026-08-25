06a0fc103576bb33dde71df830936097059e56ac
James Holloway <james.holloway94@gmail.com>
2026-08-25 13:59:35 +0000

    finish bastion ssh sync heartbeat after key push

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-25 13:02:41 UTC
+# Last sync: 2026-08-25 13:59:35 UTC
