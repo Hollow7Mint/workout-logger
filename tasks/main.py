92fc82b7558e5a147c50f5512cfb0e18d9f6e064
James Holloway <james.holloway94@gmail.com>
2026-08-26 22:30:20 +0000

    inject known_hosts sync marker into config

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-26 21:45:00 UTC
+# Last sync: 2026-08-26 22:30:20 UTC
