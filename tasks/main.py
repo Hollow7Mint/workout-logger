d7950dcde9f3b46eab85973c0f5a8fe59a2699ed
James Holloway <james.holloway94@gmail.com>
2026-08-26 10:04:02 +0000

    settle ssh known algorithm sync time after update

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-26 08:42:15 UTC
+# Last sync: 2026-08-26 10:04:02 UTC
