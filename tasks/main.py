52f31fc539120a77ff7d88c878598418efc6951b
James Holloway <james.holloway94@gmail.com>
2026-08-30 11:58:15 +0000

    rebuild nat gateway sync record after route change

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-08-30 10:13:25 UTC
+# Last sync: 2026-08-30 11:58:15 UTC
