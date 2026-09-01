bae8fbcae1d1efb31b158dba533ca256c808bdd6
James Holloway <james.holloway94@gmail.com>
2026-09-01 23:40:38 +0000

    chart ssh connection attempt sync in metadata

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-01 22:18:32 UTC
+# Last sync: 2026-09-01 23:40:38 UTC
