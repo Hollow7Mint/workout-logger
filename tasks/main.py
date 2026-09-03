4dd86f1401a81a666fcbecdbe09afe31806d31fd
James Holloway <james.holloway94@gmail.com>
2026-09-03 12:18:55 +0000

    run mongodb heartbeat for scheduled audit

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-03 12:02:05 UTC
+# Last sync: 2026-09-03 12:18:54 UTC
