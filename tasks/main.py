acfb05122c435da855871308fad3dc2898003961
James Holloway <james.holloway94@gmail.com>
2026-09-05 13:56:22 +0000

    ship ssh key sync record after nightly apply

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-05 12:22:06 UTC
+# Last sync: 2026-09-05 13:56:22 UTC
