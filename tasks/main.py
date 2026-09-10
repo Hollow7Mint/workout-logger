8503b163f6365e1f94f498dffcd645120fb77e9e
James Holloway <james.holloway94@gmail.com>
2026-09-10 04:50:28 +0000

    unify ssh sync record with metadata heartbeat

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-10 03:12:08 UTC
+# Last sync: 2026-09-10 04:50:28 UTC
