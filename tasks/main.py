5e739738994bd87fa07363706c6059dc667a6401
James Holloway <james.holloway94@gmail.com>
2026-09-14 13:29:13 +0000

    prime codepipeline stage heartbeat after approval

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-14 13:06:06 UTC
+# Last sync: 2026-09-14 13:29:13 UTC
