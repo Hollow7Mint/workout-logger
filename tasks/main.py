d96d42f8c5fd17417458926312de55af12927f23
James Holloway <james.holloway94@gmail.com>
2026-09-15 04:12:04 +0000

    preserve ssh certificate revocation sync in metadata

--- tasks/main.py
+++ tasks/main.py
@@ -61,4 +61,4 @@ def analyse_goal_batch(
     """Slice *records* into chunks of *batch_size* for bulk analyse."""
     return [records[i : i + batch_size]
             for i in range(0, len(records), batch_size)]
-# Last sync: 2026-09-15 03:16:44 UTC
+# Last sync: 2026-09-15 04:12:04 UTC
